ae092b4dfc80c72c52f8e4b1fceaebc2ba4a0140
Jay Liu <jayliu89@gmail.com>
2023-08-30 16:59:46 -0700

    created main page, created some pages for use

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+import About from "./pages/About.jsx"
+
+const router = createBrowserRouter([
+{
+  path: "/",
+  element: <App/>,
+  errorElement: <ErrorPage/>,
+  children: [
+    {
+      index: true,
+      element: <About/>,
+    }
+  ]
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
