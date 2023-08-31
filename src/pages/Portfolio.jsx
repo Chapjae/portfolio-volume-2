8f199224fac58829d4e4166d46dc66457642242a
Jay Liu <jayliu89@gmail.com>
2023-08-30 23:38:28 -0700

    created footer and added links to github, linkedin and imdb

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,17 @@
-import { Route } from "react-router-dom"
-
 const Portfolio = () => {
   return (
   <>
-  <Route path="github.com/Chapjae/sillyCode_valley" target="_blank">sillyCode_valley</Route>
+  <img></img>
+  <a href="https://github.com/Chapjae/weather-checker" target="_blank">Weather Checker Repo</a>
+  <a href="https://chapjae.github.io/weather-checker/" target="_blank">Weather Checker Deployed</a>
+
+  <img></img>
+  <a href="http://github.com/Chapjae/sillyCode_valley" target="_blank">sillyCode_valley Repo</a>
+  <a href="https://intense-sands-40473-ea199ebca0d8.herokuapp.com/" target="_blank">sillyCode_valley Deployed</a>
+  
+  <img></img>
+  <a href="https://github.com/JulianmLacey/team12Project1" target="_blank">Best Place? Repo</a>
+  <a href="https://shielded-sea-18665-6a243915e16d.herokuapp.com/" target="_blank">Best Place? Deployed</a>
   </>
     )
 }
