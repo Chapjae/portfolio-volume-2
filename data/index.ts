88dbb7fe693e2db4b7f725e90fb64524ee62a54e
Jay Liu <jayliu89@gmail.com>
2024-10-20 23:12:52 -0700

    updated experience and projects

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,7 @@
 export const navItems = [
   { name: 'About', link: '#about' },
   { name: 'Projects', link: '#projects' },
-  { name: 'Testimonials', link: '#testimonials' },
+  // { name: 'Testimonials', link: '#testimonials' },
   { name: 'Contact', link: '#contact' },
 ];
 
@@ -72,36 +72,36 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: '3D Solar System Planets to Explore',
-    des: 'Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.',
+    title: 'Kickback Hydration Product Page',
+    des: 'Product page for lemonade flavor hydration packet using Three.js.',
     img: '/p1.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/fm.svg'],
-    link: 'https://github.com/adrianhajdin?tab=repositories',
+    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg'],
+    link: 'https://github.com/APHydration/kickback',
   },
   {
     id: 2,
-    title: 'Yoom - Video Conferencing App',
-    des: 'Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.',
+    title: 'Bluefin Music Ear Training App',
+    des: 'Full stack muscial note matching game.',
     img: '/p2.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/stream.svg', '/c.svg'],
-    link: 'https://github.com/adrianhajdin/zoom-clone',
+    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
+    link: 'https://murmuring-thicket-14542-74d2838aa204.herokuapp.com/',
   },
   {
     id: 3,
-    title: 'AI Image SaaS - Canva Application',
-    des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+    title: 'Puffer Isle Resort',
+    des: 'Website for Puffer Isle Resort located in Dingalan Philippines.',
     img: '/p3.svg',
-    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
-    link: 'https://github.com/adrianhajdin/ai_saas_app',
+    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/gatsby.svg'],
+    link: 'https://pufferisleresort.netlify.app/',
   },
-  {
-    id: 4,
-    title: 'Animated Apple Iphone 3D Website',
-    des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
-    img: '/p4.svg',
-    iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
-    link: 'https://github.com/adrianhajdin/iphone',
-  },
+  // {
+  //   id: 4,
+  //   title: 'Animated Apple Iphone 3D Website',
+  //   des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
+  //   img: '/p4.svg',
+  //   iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
+  //   link: 'https://github.com/adrianhajdin/iphone',
+  // },
 ];
 
 export const testimonials = [
@@ -174,44 +174,42 @@ export const workExperience = [
   {
     id: 1,
     title: 'Frontend Engineer Intern',
-    desc: 'Assisted in the development of a web-based platform using React.js, enhancing interactivity.',
+    desc: 'Assisted in the development of a web-based platform using React.js and Three.js, enhancing interactivity.',
     className: 'md:col-span-2',
     thumbnail: '/exp1.svg',
   },
-  {
-    id: 2,
-    title: 'Mobile App Dev - JSM Tech',
-    desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp2.svg',
-  },
-  {
-    id: 3,
-    title: 'Freelance App Dev Project',
-    desc: 'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
-    className: 'md:col-span-2', // change to md:col-span-2
-    thumbnail: '/exp3.svg',
-  },
-  {
-    id: 4,
-    title: 'Lead Frontend Developer',
-    desc: 'Developed and maintained user-facing features using modern frontend technologies.',
-    className: 'md:col-span-2',
-    thumbnail: '/exp4.svg',
-  },
+  // {
+  //   id: 2,
+  //   title: 'Mobile App Dev - JSM Tech',
+  //   desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
+  //   className: 'md:col-span-2', // change to md:col-span-2
+  //   thumbnail: '/exp2.svg',
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Freelance App Dev Project',
+  //   desc: 'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
+  //   className: 'md:col-span-2', // change to md:col-span-2
+  //   thumbnail: '/exp3.svg',
+  // },
+  // {
+  //   id: 4,
+  //   title: 'Lead Frontend Developer',
+  //   desc: 'Developed and maintained user-facing features using modern frontend technologies.',
+  //   className: 'md:col-span-2',
+  //   thumbnail: '/exp4.svg',
+  // },
 ];
 
 export const socialMedia = [
   {
     id: 1,
     img: '/git.svg',
+    url: 'https://github.com/chapjae',
   },
   {
     id: 2,
-    img: '/twit.svg',
-  },
-  {
-    id: 3,
     img: '/link.svg',
+    url: 'https://www.linkedin.com/in/jay-liu-3208a3bb/',
   },
 ];
